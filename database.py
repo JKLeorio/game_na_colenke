257b7bdfe7bf60b4cc309efdd4f4cf4ed46c3b3f
JKLeorio <jkdplatinum@gmail.com>
2021-12-30 23:43:28 +0600

    fix changes

--- database.py
+++ database.py
@@ -1,26 +1,35 @@
 import sqlite3
-db= sqlite3.connect("2048.sqlite")
-cur=db.cursor()
+
+db = sqlite3.connect("2048.sqlite")
+cur = db.cursor()
+
 
 def get_best():
-    cur.execute("""
+    cur.execute(
+        """
     create table if not exists RECORDS (
         name text,
         score integer
     )
-    """)
+    """
+    )
 
-    cur.execute(''' 
+    cur.execute(
+        """ 
     SELECT name, max(score) score FROM RECORDS
     GROUP by name
     ORDER by score DESC
     LIMIT 3 
-    ''')
-    return(cur.fetchall())
+    """
+    )
+    return cur.fetchall()
 
-def add_player(player_name,player_score):
-    cur.execute(f'''
+
+def add_player(player_name, player_score):
+    cur.execute(
+        f"""
     INSERT INTO RECORDS(name,score)
     VALUES("{player_name}","{player_score}")
-    ''')
+    """
+    )
     db.commit()
--- program_logic.py
+++ program_logic.py
@@ -1,34 +1,41 @@
 import random
 import copy
-def get_numbers_from_index(row,column):
-    return row*4+column+1
+
+
+def get_numbers_from_index(row, column):
+    return row * 4 + column + 1
+
 
 def pretty_print(mas):
-    print('-'*10)
+    print("-" * 10)
     for row in mas:
         print(*row)
-    print('-'*10)
+    print("-" * 10)
+
 
 def get_index_for_number(num):
-    num-=1
-    x,y=num//4, num%4
-    return x,y
+    num -= 1
+    x, y = num // 4, num % 4
+    return x, y
 
-def insert_2_or_4(mas,x,y):
+
+def insert_2_or_4(mas, x, y):
     if random.random() >= 0.75:
-        mas[x][y]=4
+        mas[x][y] = 4
     else:
-        mas[x][y]=2
+        mas[x][y] = 2
     return mas
 
+
 def get_empty_list(mas):
-    empty=[]
-    for row in range(0,4):
-        for column in range(0,4):
-            if mas[row][column]==0:
-                num= get_numbers_from_index(row,column)
+    empty = []
+    for row in range(0, 4):
+        for column in range(0, 4):
+            if mas[row][column] == 0:
+                num = get_numbers_from_index(row, column)
                 empty.append(num)
     return empty
+
 
 def is_zero_in_mas(mas):
     for row in mas:
@@ -36,9 +43,10 @@ def is_zero_in_mas(mas):
             return True
     return False
 
+
 def move_left(mas):
-    delta=0
-    origin=copy.deepcopy(mas)
+    delta = 0
+    origin = copy.deepcopy(mas)
     for row in mas:
         while 0 in row:
             row.remove(0)
@@ -46,89 +54,96 @@ def move_left(mas):
             row.append(0)
     for i in range(4):
         for j in range(3):
-            if mas[i][j]==mas[i][j+1] and mas[i][j]!=0:
-                mas[i][j]*=2
-                delta+=mas[i][j]
-                mas[i].pop(j+1)
+            if mas[i][j] == mas[i][j + 1] and mas[i][j] != 0:
+                mas[i][j] *= 2
+                delta += mas[i][j]
+                mas[i].pop(j + 1)
                 mas[i].append(0)
-    return mas,delta, not origin == mas
+    return mas, delta, not origin == mas
+
+
 def move_right(mas):
-    delta=0
-    origin=copy.deepcopy(mas)
+    delta = 0
+    origin = copy.deepcopy(mas)
     for row in mas:
         while 0 in row:
             row.remove(0)
         while len(row) != 4:
-            row.insert(0,0)
+            row.insert(0, 0)
     for i in range(4):
-        for j in range(3,0,-1):
-            if mas[i][j]==mas[i][j-1] and mas[i][j]!=0:
-                mas[i][j]*=2
-                delta+=mas[i][j]
-                mas[i].pop(j-1)
-                mas[i].insert(0,0)
-    return mas,delta,not origin == mas
+        for j in range(3, 0, -1):
+            if mas[i][j] == mas[i][j - 1] and mas[i][j] != 0:
+                mas[i][j] *= 2
+                delta += mas[i][j]
+                mas[i].pop(j - 1)
+                mas[i].insert(0, 0)
+    return mas, delta, not origin == mas
+
+
 def move_up(mas):
-    delta=0
-    origin=copy.deepcopy(mas)
+    delta = 0
+    origin = copy.deepcopy(mas)
     for i_2 in range(2):
         for j_2 in range(4):
-            if mas[i_2+1][j_2]==0 and mas[i_2+2][j_2]!=0:
-                mas[i_2+1][j_2]=mas[i_2+2][j_2]
-                mas[i_2+2][j_2]=0
-            if mas[i_2][j_2]==0:
-                mas[i_2][j_2]=mas[i_2+1][j_2]
-                mas[i_2+1][j_2]=0
+            if mas[i_2 + 1][j_2] == 0 and mas[i_2 + 2][j_2] != 0:
+                mas[i_2 + 1][j_2] = mas[i_2 + 2][j_2]
+                mas[i_2 + 2][j_2] = 0
+            if mas[i_2][j_2] == 0:
+                mas[i_2][j_2] = mas[i_2 + 1][j_2]
+                mas[i_2 + 1][j_2] = 0
     for row in range(3):
         for column in range(4):
-            if mas[row][column]== mas[row+1][column]:
-                mas[row][column]*=2
-                delta+=mas[row][column]
-                mas[row+1][column]=0
+            if mas[row][column] == mas[row + 1][column]:
+                mas[row][column] *= 2
+                delta += mas[row][column]
+                mas[row + 1][column] = 0
     for i in range(2):
         for j in range(4):
-            if mas[i+1][j]==0 and mas[i+2][j]!=0:
-                mas[i+1][j]=mas[i+2][j]
-                mas[i+2][j]=0
-            if mas[i][j]==0:
-                mas[i][j]=mas[i+1][j]
-                mas[i+1][j]=0
-    return mas,delta,not origin == mas
+            if mas[i + 1][j] == 0 and mas[i + 2][j] != 0:
+                mas[i + 1][j] = mas[i + 2][j]
+                mas[i + 2][j] = 0
+            if mas[i][j] == 0:
+                mas[i][j] = mas[i + 1][j]
+                mas[i + 1][j] = 0
+    return mas, delta, not origin == mas
+
+
 def move_down(mas):
-    delta=0
-    origin=copy.deepcopy(mas)
-    for i_2 in range(4,1,-1):
+    delta = 0
+    origin = copy.deepcopy(mas)
+    for i_2 in range(4, 1, -1):
         for j_2 in range(4):
-            if mas[i_2-1][j_2]==0 and mas[i_2-2][j_2]!=0:
-                mas[i_2-1][j_2]=mas[i_2-2][j_2]
-                mas[i_2-2][j_2]=0
-    for i_3 in range(4,1,-1):
+            if mas[i_2 - 1][j_2] == 0 and mas[i_2 - 2][j_2] != 0:
+                mas[i_2 - 1][j_2] = mas[i_2 - 2][j_2]
+                mas[i_2 - 2][j_2] = 0
+    for i_3 in range(4, 1, -1):
         for j_3 in range(4):
-            if mas[i_3-1][j_3]==0 and mas[i_3-2][j_3]!=0:
-                mas[i_3-1][j_3]=mas[i_3-2][j_3]
-                mas[i_3-2][j_3]=0
-    for row in range(4,1,-1):
+            if mas[i_3 - 1][j_3] == 0 and mas[i_3 - 2][j_3] != 0:
+                mas[i_3 - 1][j_3] = mas[i_3 - 2][j_3]
+                mas[i_3 - 2][j_3] = 0
+    for row in range(4, 1, -1):
         for column in range(4):
-            if mas[row-1][column]==mas[row-2][column]:
-                mas[row-1][column]*=2
-                delta+=mas[row-1][column]
-                mas[row-2][column]=0
-    for i in range(4,1,-1):
+            if mas[row - 1][column] == mas[row - 2][column]:
+                mas[row - 1][column] *= 2
+                delta += mas[row - 1][column]
+                mas[row - 2][column] = 0
+    for i in range(4, 1, -1):
         for j in range(4):
-            if mas[i-1][j]==0 and mas[i-2][j]!=0:
-                mas[i-1][j]=mas[i-2][j]
-                mas[i-2][j]=0
-    return mas,delta,not origin == mas
+            if mas[i - 1][j] == 0 and mas[i - 2][j] != 0:
+                mas[i - 1][j] = mas[i - 2][j]
+                mas[i - 2][j] = 0
+    return mas, delta, not origin == mas
+
 
 def can_move(mas):
-    one=copy.deepcopy(mas)
-    two=copy.deepcopy(mas)
-    three=copy.deepcopy(mas)
-    four=copy.deepcopy(mas)
-    x,y,trash = move_right(one)
-    t,b,trash = move_left(two)
-    g,h,trash = move_up(three)
-    n,v,trash = move_down(four)
+    one = copy.deepcopy(mas)
+    two = copy.deepcopy(mas)
+    three = copy.deepcopy(mas)
+    four = copy.deepcopy(mas)
+    x, y, trash = move_right(one)
+    t, b, trash = move_left(two)
+    g, h, trash = move_up(three)
+    n, v, trash = move_down(four)
     if mas == x and mas == t and mas == g and mas == n:
         return False
     else:
